a104f8270da39095baaee2afb72b3fa3c5a526c1
Noah Zarro <zarro@ubique.ch>
2025-01-10 14:22:19 +0100

    edited logging format

--- adbinstall.sh
+++ adbinstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 source venv/bin/activate
+echo >> log.log
 date >> log.log
 PATH=$PATH:/mnt/c/Users/noahz/AppData/Local/Android/Sdk/platform-tools
 python main.py >> log.log 2>&1
